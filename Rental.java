2b3b9ec34a820654d58abbeef3938dc57baaa7e4
Kimmoonsu <visitant2@naver.com>
2021-11-17 15:21:29 +0900

    Dispensable - Speculative generality

--- Rental.java
+++ Rental.java
@@ -16,10 +16,6 @@ public class Rental {
 		return video;
 	}
 
-	public void setVideo(Video video) {
-		this.video = video;
-	}
-
 	public boolean isRented() {
 		return video.isRented();
 	}
@@ -40,16 +36,8 @@ public class Rental {
 		return rentDate;
 	}
 
-	public void setRentDate(Date rentDate) {
-		this.rentDate = rentDate;
-	}
-
 	public Date getReturnDate() {
 		return returnDate;
-	}
-
-	public void setReturnDate(Date returnDate) {
-		this.returnDate = returnDate;
 	}
 
 	public int getDaysRentedLimit() {
